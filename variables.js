ff5bd04ee7f37f78c98654283b8261b2f4966926
Cleveland <ayocleveland@gmail.com>
2022-02-24 20:24:51 -0700

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,12 +1,20 @@
 var greetings = 'Hello how are you';
+console.log 'greetings';
 
 // variable ballGames is declared 'You can play tennis'
 var ballGames = 'You can play tennis';
+console.log 'ballGames';
+
 
 var jerseyNumber = 72;
+console.log 'jerseyNumeber';
 
 var numberOfPets = 3;
+console.log 'numberOfPets';
 
 var sleepy = true;
+console.log 'sleepy'
 
 var asleep = false;
+console.log 'asleep';
+
